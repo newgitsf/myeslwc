a21fbff6cdf311b5da407eb6fa129cd945b16ab7
Abhishek Kumar <abhishekkmr251@gmail.com>
2020-08-06 13:01:03 +0530

    added Apex Classes

---
+++ es-base-objects/main/default/lwc/customerTile/customerTile.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+export default class CustomerTile extends LightningElement {
+    @api customer;
+    @api object;
+
+    connectedCallBack() {
+
+    }
+
+    get icon() {
+        return 'doctype:image';
+    }
+
+    get alttext() {
+        return 'image';
+    }
+}
